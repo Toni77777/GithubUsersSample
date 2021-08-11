5181d22456e719a3babce599e8e0de897b2c6286
Anton Paliakou <toxa95401@gmail.com>
2021-08-12 00:03:35 +0300

    Init project.

---
+++ GithubUsers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubUsers
+//
+//  Created by Anton Paliakou on 8/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubUsers/GithubUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubUsersApp.swift
+//  GithubUsers
+//
+//  Created by Anton Paliakou on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct GithubUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
